69a02f188cb1883807c3153d430a62d488aad20b
Pradhuman Goyal <pradhumangoyal@users.noreply.github.com>
2020-10-02 23:07:21 +0530

    FiveDigitCounter

--- script.js
+++ script.js
@@ -1,41 +1,71 @@
-var box = document.getElementById("box");
-
-var viewWidth = window.innerWidth;
-var viewHeight = window.innerHeight;
+var countInterval;
 
 
-// Updates the viewport height and width dynamically
-window.addEventListener("resize", function(event) {
-    viewWidth = window.innerWidth;
-    viewHeight = window.innerHeight;
-});
-
-
-
-box.addEventListener("mouseover", function(event) {
-    var boxAttr = box.getBoundingClientRect();
+function startCounter() {
     
-    var pos = getNewPosition(boxAttr.width, boxAttr.height);
+    var number = parseInt(document.getElementById("number").value);
     
-    box.style.top = pos.y + "px";
-    box.style.left = pos.x + "px";
-});
-
-
-
-function getNewPosition(boxWidth, boxHeight) {
-    
-    // The boxWidth and boxHeight are subtracted so that they would not move out from the right and bottom direction
-    var newX = Math.floor((Math.random() * viewWidth) + 1 - boxWidth);
-    var newY = Math.floor((Math.random() * viewHeight) + 1 - boxHeight);
-
-    // These will satisfy that box does not move go out in the top and left direction
-    if(newX < 0) {
-        newX = 0;
+    if(isNaN(number)) {
+        alert("Please enter a number");
+        clearInterval(countInterval);   // This is important for the condition when a counter is running and you entered a wrong input for a new counter
+        return;
     }
-    if(newY < 0) {
-        newY = 0;
+    if(number < 1 || number > 99999) {
+        alert("Range out of bounds");
+        clearInterval(countInterval);
+        return;
     }
     
-    return {x: newX, y: newY};
+    var currentNos = document.querySelectorAll(".counter .current");
+    var nextNos = document.querySelectorAll(".counter .next");
+    var count = 0;
+    
+    // If user clicks on 'Start Counter' button again - remove this function and below line if you don't consider this situation
+    resetNumbers(currentNos, nextNos, 5);
+    
+    // Clears the previous interval that was running
+    clearInterval(countInterval);
+    
+    countInterval = setInterval(function() {
+        if(count === number) {
+            clearInterval(countInterval);
+            alert("Counter has stopped");
+            return;
+        }
+        increaseCount(currentNos, nextNos, 4);
+        count++;
+    }, 1000);
+
+}
+
+
+function resetNumbers(currentNos, nextNos, end) {
+    for(var i=0; i<end; ++i) {
+        currentNos[i].innerText = 0;
+        nextNos[i].innerText = 1;
+    }
+}
+
+
+
+function increaseCount(currentNos, nextNos, index) {
+    
+    let current = currentNos[index];
+    let next = nextNos[index];
+    
+    if(current.innerText == 9) {
+        increaseCount(currentNos, nextNos, index-1);
+    }
+    
+    next.classList.add("animate");
+    
+    setTimeout(function() {
+        current.innerText = next.innerText;
+        next.classList.remove("animate");
+        next.innerText = parseInt(next.innerText) + 1;
+        if(next.innerText > 9) {
+            next.innerText = 0;
+        }
+    }, 500);
+    
 }
